735269609133dcb9e8e81c614598c06ed8650033
USER <USER@DESKTOP-4G46P0B>
2026-02-09 18:16:46 +0300

    Add project files.

---
+++ Assignment for bill/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.WriteLine("Please Enter Your Bill "); 
+double billAmount = Convert.ToDouble(Console.ReadLine()); //input from  user and convert to double 
+double discount = 0;
+if  (billAmount >= 500)
+{
+    discount= billAmount - (billAmount * 0.20);
+}
+else if  (billAmount >= 300 && billAmount <= 499)
+{
+    discount = billAmount -  (billAmount * 0.10);
+}
+
+else
+{
+    discount= billAmount;
+}
+Console.WriteLine($"The Final Price : {discount}  ");
+
+
